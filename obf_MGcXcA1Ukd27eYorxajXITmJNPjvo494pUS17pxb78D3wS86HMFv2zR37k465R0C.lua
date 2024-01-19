f280bdf7f2de7561ab790cdf6440e1ddbdab08e0
vl0526 <156920571+vl0526@users.noreply.github.com>
2024-01-19 17:27:27 +0700

    Add files via upload

---
+++ obf_MGcXcA1Ukd27eYorxajXITmJNPjvo494pUS17pxb78D3wS86HMFv2zR37k465R0C.lua
@@ -0,0 +1,5 @@
+---// Join MTien Hub Sever To Update New Script \\--
+--// Sever Discord: https://discord.com/invite/pCBDH5z382 \\--
+_G.Name = "hao hub" -- Put You Name Script Here
+_G.Logo = "1677466812" -- Enter Your ID Image Here
+loadstring(Game:HttpGet"https://raw.githubusercontent.com/ImTienNguyenZ/NguyenTienSimpRose/main/NguyenMinhTienLoader")()
